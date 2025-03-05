baa276e0b49fc6706579c11cb03f0f325e188316
Bikashkang <37011760+Bikashkang@users.noreply.github.com>
2025-03-05 23:44:13 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    // Ensure this points to your source code
+    './app/**/*.{js,tsx,ts,jsx}',
+    // If you use a `src` directory, add: './src/**/*.{js,tsx,ts,jsx}'
+    // Do the same with `components`, `hooks`, `styles`, or any other top-level directories
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
